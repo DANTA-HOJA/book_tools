aa52a4d9011157ae4fb3e144ad5efc85b1f9684d
Ryan Yang <rime97410000@gmail.com>
2025-02-04 05:12:20 +0800

    [fn: new] `check_dir()`

--- auto_backfill_registered_id/modules/check.py
+++ auto_backfill_registered_id/modules/check.py
@@ -6,8 +6,16 @@ def check_xlsx(file: Path, _class_name: str):
     """
     """
     if not file.exists():
-        raise ValueError(f"指定的 '{_class_name}' 不存在: '{file}'")
+        raise ValueError(f"指定的 '{_class_name}' 不存在, Current path: '{file}'")
     
     if file.suffix != ".xlsx":
-        raise ValueError(f"指定的 '{_class_name}' 不是 Excel 檔: '{file}'")
+        raise ValueError(f"指定的 '{_class_name}' 不是 Excel 檔, Current path: '{file}'")
+    # -------------------------------------------------------------------------/
+
+
+def check_dir(dir: Path):
+    """
+    """
+    if not dir.is_dir():
+        raise ValueError(f"指定的存放路徑必須是資料夾, Current path: '{dir}'")
     # -------------------------------------------------------------------------/
